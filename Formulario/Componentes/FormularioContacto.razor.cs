33994e899fe2dfb766e4cc303eca439be6a630a6
gbauluz <gbauluz@esypro.com>
2024-12-13 13:24:23 +0100

    Ej 1.2 Final v2

--- Formulario/Componentes/FormularioContacto.razor.cs
+++ Formulario/Componentes/FormularioContacto.razor.cs
@@ -9,9 +9,9 @@ namespace Formulario.Componentes
     public partial class FormularioContacto
     {
 
-        private ContactoForm _contact = new();
-        private EditContext? _editContext;
-        private ValidationMessageStore? _validationMessageStore;
+        private readonly ContactoForm _contact = new();
+        private EditContext _editContext=default!;
+        private ValidationMessageStore _validationMessageStore = default!;
 
         [Parameter]
         public EventCallback<ContactoForm> EnviarCallBack { get; set; }
@@ -27,7 +27,7 @@ namespace Formulario.Componentes
 
         public void Enviar()
         {
-            ContactoFormValidation(_contact);
+            ContactoFormValidation();
             bool isValid = _editContext.Validate();
 
             if (isValid)
@@ -38,61 +38,91 @@ namespace Formulario.Componentes
 
         }
 
-        private void ContactoFormValidation(ContactoForm contacto)
+        private void ContactoFormValidation()
         {
             _validationMessageStore.Clear();
 
-            var properties = typeof(ContactoForm).GetProperties();
-
-
-            foreach (var property in properties)
+            if (string.IsNullOrWhiteSpace(_contact.Nombre))
             {
-                var value = property.GetValue(contacto) as string;
-                if (string.IsNullOrWhiteSpace(value))
+                _validationMessageStore.Add(_editContext.Field(nameof(ContactoForm.Nombre)), $"El campo es obligatorio");
+            }
+
+
+            if (string.IsNullOrWhiteSpace(_contact.Apellidos))
+            {
+                _validationMessageStore.Add(_editContext.Field(nameof(ContactoForm.Apellidos)), $"El campo es obligatorio");
+            }
+
+
+            if (string.IsNullOrWhiteSpace(_contact.Correo))
+            {
+                _validationMessageStore.Add(_editContext.Field(nameof(ContactoForm.Correo)), $"El campo es obligatorio");
+            }
+            else
+            {
+                ContactoFormEmailValidation();
+            }
+
+
+            if (string.IsNullOrWhiteSpace(_contact.Password))
+            {
+                _validationMessageStore.Add(_editContext.Field(nameof(ContactoForm.Password)), $"El campo es obligatorio");
+            }
+            else
+            {
+                if (ContactoFormPasswordValidation())
                 {
-                    _validationMessageStore.Add(_editContext.Field(property.Name), $"El campo {property.Name} es obligatorio");
-                }
-                else if (property.Name == "Correo")
-                {
-                    ContactoFormEmailValidation(contacto);
-                }
-                else if (property.Name == "Password")
-                {
-                    ContactoFormPasswordValidation(contacto);
-                }
-                else if (property.Name == "RepetirPassword")
-                {
-                    ContactoFormMatchedPasswordsValidation(contacto);
+                    if (string.IsNullOrWhiteSpace(_contact.RepetirPassword))
+                    {
+                        _validationMessageStore.Add(_editContext.Field(nameof(ContactoForm.RepetirPassword)), $"El campo es obligatorio");
+                    }
+                    else
+                    {
+                        ContactoFormMatchedPasswordsValidation();
+                    }
                 }
             }
-            _editContext.NotifyValidationStateChanged();
 
+
+            if (string.IsNullOrWhiteSpace(_contact.Nickname))
+            {
+                _validationMessageStore.Add(_editContext.Field(nameof(ContactoForm.Nickname)), $"El campo es obligatorio");
+            }
+
+            _editContext.NotifyValidationStateChanged();
         }
 
-        private void ContactoFormEmailValidation(ContactoForm contacto)
+
+
+        private void ContactoFormEmailValidation()
         {
             var emailRegex = new Regex("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$");
-            if (!emailRegex.IsMatch(contacto.Correo))
+            if (!emailRegex.IsMatch(_contact.Correo))
             {
-                _validationMessageStore.Add(_editContext.Field("Correo"), "El correo no tiene un formato válido");
+                _validationMessageStore.Add(_editContext.Field(nameof(ContactoForm.Correo)), "El correo no tiene un formato válido");
             }
         }
 
-        private void ContactoFormPasswordValidation(ContactoForm contacto)
+
+        private bool ContactoFormPasswordValidation()
         {
+            bool isValid = true;
             var psswdRegex = new Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{4,}$");
-            if (!psswdRegex.IsMatch(contacto.Password))
+            if (!psswdRegex.IsMatch(_contact.Password))
             {
-                _validationMessageStore.Add(_editContext.Field("Password"), "La contraseña debe contener al menos 4 caracteres, incluyendo una letra mayúscula, una letra minúscula, un número y un carácter especial");
+                _validationMessageStore.Add(_editContext.Field(nameof(ContactoForm.Password)), "La contraseña debe contener al menos 4 caracteres, incluyendo una letra mayúscula, una letra minúscula, un número y un carácter especial");
+                isValid = false;
             }
+            return isValid;
         }
 
-        private void ContactoFormMatchedPasswordsValidation(ContactoForm contacto)
+
+        private void ContactoFormMatchedPasswordsValidation()
         {
 
-            if (contacto.Password != contacto.RepetirPassword)
+            if (_contact.RepetirPassword != _contact.Password)
             {
-                _validationMessageStore.Add(_editContext.Field("RepetirPassword"), "Las contraseñas deben coincidir");
+                _validationMessageStore.Add(_editContext.Field(nameof(ContactoForm.RepetirPassword)), "Las contraseñas deben coincidir");
             }
         }
 
